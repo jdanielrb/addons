4d5f9e71741d7ad6c0dc0d17d57d349da6db6c8d
Jose Daniel Ramirez Bastidas <danik_mi@hotmail.com>
2024-07-30 21:13:45 -0700

    update

---
+++ contenido_incrustado/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Herramientas WTECH Ciudad Segura",
+    "author": "WTECH Ciudad Segura",
+    "version": "1.0",
+    "category": "tools",
+    # "depends": ["base"],
+    "data": [
+        "views/01_graficos_views.xml",  # Primero la vista
+        "views/02_pdf_views.xml",  # Primero la vista
+        "views/herramientas_actions.xml",  # Luego la acción
+        "views/herramientas_menu.xml",  # Y al final el menú
+    ],
+        'assets': {
+        'web.assets_backend': [
+            'contenido_incrustado/static/src/css/styles.css',  # (Opcional) Para estilos adicionales
+        ],
+    },
+    "installable": True,
+    "application": True,
+}
