1b29b12014431a881668c82dddd0b85e50c12866
Jose Daniel Ramirez Bastidas <danik_mi@hotmail.com>
2024-08-05 23:26:58 -0700

    a

--- contenido_incrustado/static/src/js/ocultar_control_panel.js
+++ contenido_incrustado/static/src/js/ocultar_control_panel.js
@@ -1,10 +1,20 @@
-odoo.define('contenido_incrustado.ocultar_control_panel', function (require) {
-    "use strict";
-    var ControlPanel = require('web.ControlPanel');
+/** @odoo-module **/
 
-    // ControlPanel.include({
-    //     _renderContent: function () {
-    //         return $();
-    //     }
-    // });
-});
+import { registry } from "@web/core/registry";
+import { ListView } from "@web/views/list/list_view";
+
+function removeControlPanel(env) {
+    const components = registry.category("views").get("list");
+    if (components) {
+        components.forEach((component) => {
+            const originalSetup = component.prototype.setup;
+            component.prototype.setup = function () {
+                const res = originalSetup.call(this);
+                this.props.controlPanelProps.hidden = true;  // Ocultar el control panel
+                return res;
+            };
+        });
+    }
+}
+
+removeControlPanel();
