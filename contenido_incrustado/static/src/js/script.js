1febed5a10ff270f630291ef0b7dc3db834d1a69
Jose Daniel Ramirez Bastidas <danik_mi@hotmail.com>
2024-08-05 23:06:01 -0700

    a

--- contenido_incrustado/static/src/js/script.js
+++ contenido_incrustado/static/src/js/script.js
@@ -2,9 +2,9 @@ odoo.define('contenido_incrustado.script', function (require) {
     "use strict";
     var ControlPanel = require('web.ControlPanel');
 
-    ControlPanel.include({
-        _renderContent: function () {
-            return $();
-        }
-    });
+    // ControlPanel.include({
+    //     _renderContent: function () {
+    //         return $();
+    //     }
+    // });
 });
