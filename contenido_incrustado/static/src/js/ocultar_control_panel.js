822b3daed1658eb9bdd58632d87f1fdd66b5c546
Jose Daniel Ramirez Bastidas <danik_mi@hotmail.com>
2024-08-05 23:15:05 -0700

    a

---
+++ contenido_incrustado/static/src/js/ocultar_control_panel.js
@@ -0,0 +1,10 @@
+odoo.define('contenido_incrustado.ocultar_control_panel', function (require) {
+    "use strict";
+    var ControlPanel = require('web.ControlPanel');
+
+    // ControlPanel.include({
+    //     _renderContent: function () {
+    //         return $();
+    //     }
+    // });
+});
