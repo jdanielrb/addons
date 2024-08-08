9642e7ca7e619bbdc1ca631c2559d4ddfa210bd1
jdanielrb <157528248+jdanielrb@users.noreply.github.com>
2024-08-08 10:50:57 -0700

    update

--- herramientas_wtech/__manifest__.py
+++ herramientas_wtech/__manifest__.py
@@ -1,35 +1,19 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "my_module",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
+    "name": "Herramientas WTECH Ciudad Segura",
+    "author": "WTECH Ciudad Segura",
+    "version": "1.0",
+    "category": "tools",
+    "depends": ["base"],
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        "views/views.xml",
+        "views/templates.xml",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "assets": {
+        "web.assets_backend": [
+            "contenido_incrustado/static/src/css/styles.css",
+        ],
+    },
+    "installable": True,
+    "application": True,
 }
