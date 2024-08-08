673db9d71631126917312043839372af32a061ac
jdanielrb <157528248+jdanielrb@users.noreply.github.com>
2024-08-08 10:10:19 -0700

    a

--- contenido_incrustado/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import contenido_incrustado
--- contenido_incrustado/models/contenido_incrustado.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class ContenidoIncrustado(models.Model):
-    _name = 'contenido.incrustado'
-    _description = 'Vista para contenido incrustado'
-
-    name = fields.Char(string='Nombre', default='Iframe Content')
